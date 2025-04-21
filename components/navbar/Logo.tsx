675d3458eb4cef9819cdcb6451eddf8d9124fc08
yuna <yuna744@gmail.com>
2025-04-21 16:45:55 +0900

    Logo and NavSearch Components

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,5 +1,18 @@
+import { Button } from '../ui/button'
+import Link from 'next/link'
+import { LuTent } from 'react-icons/lu'
+
 function Logo() {
-  return <div>Logo</div>
+  return (
+    <Button
+      size='icon'
+      asChild
+    >
+      <Link href='/'>
+        <LuTent className='h-6 w-6' />
+      </Link>
+    </Button>
+  )
 }
 
 export default Logo
--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,5 +1,13 @@
+import { Input } from '../ui/input'
+
 function NavSearch() {
-  return <div>NavSearch</div>
+  return (
+    <Input
+      type='text'
+      placeholder='find a property...'
+      className='max-w-xs dark:bg-muted'
+    />
+  )
 }
 
 export default NavSearch
