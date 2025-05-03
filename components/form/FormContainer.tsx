6fcaee874c5ac7041d1b4e5c3a8b5fb980019578
yuna <yuna744@gmail.com>
2025-05-04 01:20:37 +0900

    FormContainer Component - 폼 상태 및 토스트 처리 포함 - 서버 액션 기반 컴포넌트 구현

---
+++ components/form/FormContainer.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { useToast } from '@/hooks/use-toast'
+import { actionFunction } from '@/utils/types'
+import { useEffect } from 'react'
+import { useFormState } from 'react-dom'
+
+const initialState = {
+  message: ''
+}
+
+function FormContainer({ action, children }: { action: actionFunction; children: React.ReactNode }) {
+  const [state, formAction] = useFormState(action, initialState)
+  const { toast } = useToast()
+
+  useEffect(() => {
+    if (state.message) {
+      toast({ description: state.message })
+    }
+  }, [state])
+
+  return <form action={formAction}>{children}</form>
+}
+
+export default FormContainer
