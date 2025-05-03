83c3f52404879ffbee3e2e13744d29b47c6ea299
yuna <yuna744@gmail.com>
2025-05-04 01:39:34 +0900

    Refactor Create Profile - 프로필 생성 페이지 구성 및 서버 액션 연동

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,35 +1,46 @@
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
+import FormContainer from '@/components/form/FormContainer'
+import { SubmitButton } from '@/components/form/SubmitButton'
+import FormInput from '@/components/form/formInput'
 
-const createProfileAction = async (formData: FormData) => {
+const createProfileAction = async (prevState: any, formData: FormData) => {
   'use server'
   const firstName = formData.get('firstName') as string
   console.log(firstName)
+  if (firstName !== 'shakeAndBake') return { message: 'There was an error...' }
+  return { message: 'Profile Created' }
 }
 
 function CreateProfilePage() {
   return (
     <section>
       <h1 className='mb-8 text-2xl font-semibold capitalize'>new user</h1>
-      <form action={createProfileAction}>
-        <div className='max-w-lg rounded-md border p-8'>
-          <div className='mb-2'>
-            <Label htmlFor='firstName'>First Name</Label>
-            <Input
-              id='firstName'
-              name='firstName'
+
+      <div className='rounded-md border p-8'>
+        <FormContainer action={createProfileAction}>
+          <div className='mt-4 grid gap-4 md:grid-cols-2'>
+            <FormInput
               type='text'
+              name='firstName'
+              label='First Name'
+            />
+
+            <FormInput
+              type='text'
+              name='lastName'
+              label='Last Name'
+            />
+            <FormInput
+              type='text'
+              name='userName'
+              label='User Name'
             />
           </div>
-          <Button
-            type='submit'
-            size='lg'
-          >
-            Create Profile
-          </Button>
-        </div>
-      </form>
+          <SubmitButton
+            text='Create Profile'
+            className='mt-8'
+          />
+        </FormContainer>
+      </div>
     </section>
   )
 }
