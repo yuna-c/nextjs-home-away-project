9783be6032bf1727aafe9b4c71c971ef27e2e840
yuna <yuna744@gmail.com>
2025-05-09 23:24:58 +0900

    Fetch Profile Image Action - Clerk 사용자 기반 프로필 이미지 조회 - DB에서 로그인 된 clerkId로 프로필 사진 가져오기

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,6 +1,16 @@
+import { fetchProfileImage } from '@/utils/actions'
 import { LuUser } from 'react-icons/lu'
 
-function UserIcon() {
+async function UserIcon() {
+  const profileImage = await fetchProfileImage()
+  if (profileImage) {
+    return (
+      <img
+        src={profileImage}
+        className='h-6 w-6 rounded-full object-cover'
+      />
+    )
+  }
   return <LuUser className='h-6 w-6 rounded-full bg-primary text-white' />
 }
 
