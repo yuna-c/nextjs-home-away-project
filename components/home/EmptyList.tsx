07f54b152abb3a8c807119fb14645f308273b387
yuna <yuna744@gmail.com>
2025-05-18 01:02:48 +0900

    Empty List - 숙소 없을 때 보여줄 안내 컴포넌트

--- components/home/EmptyList.tsx
+++ components/home/EmptyList.tsx
@@ -1,5 +1,28 @@
-function EmptyList() {
-  return <div>EmptyList</div>
+import { Button } from '../ui/button'
+import Link from 'next/link'
+
+function EmptyList({
+  heading = 'No items in the list.',
+  message = 'Keep exploring our properties',
+  btnText = 'back home'
+}: {
+  heading?: string
+  message?: string
+  btnText: string
+}) {
+  return (
+    <div className='mt-4'>
+      <h2 className='text-xl font-bold'>{heading}</h2>
+      <p className='text-lg'>{message}</p>
+      <Button
+        asChild
+        className='mt-4 capitalize'
+        size='lg'
+      >
+        <Link href='/'>{btnText}</Link>
+      </Button>
+    </div>
+  )
 }
 
 export default EmptyList
