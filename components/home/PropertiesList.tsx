c32c8f5a5d3b76567d41b2aad7b46739c3a530ed
yuna <yuna744@gmail.com>
2025-05-18 23:41:34 +0900

    PropertyCard - Structure - PropertiesList grid 반응형 설정

--- components/home/PropertiesList.tsx
+++ components/home/PropertiesList.tsx
@@ -3,7 +3,7 @@ import type { PropertyCardProps } from '@/utils/types'
 
 function PropertiesList({ properties }: { properties: PropertyCardProps[] }) {
   return (
-    <section>
+    <section className='mt-4 grid gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
       {properties.map((property) => {
         return (
           <PropertyCard
