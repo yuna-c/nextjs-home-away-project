0ea79cd8b16f77438273ae301414edf0e140d2ad
yuna <yuna744@gmail.com>
2025-05-22 20:41:07 +0900

    Loading Cards - Skeleton UI 제작

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import LoadingCards from '@/components/card/LoadingCards'
 import CategoriesList from '@/components/home/CategoriesList'
 import PropertiesContainer from '@/components/home/PropertiesContainer'
+import { Suspense } from 'react'
 
 function HomePage({ searchParams }: { searchParams: { category?: string; search?: string } }) {
   console.log(searchParams)
@@ -10,10 +12,12 @@ function HomePage({ searchParams }: { searchParams: { category?: string; search?
         category={searchParams.category}
         search={searchParams.search}
       />
-      <PropertiesContainer
-        category={searchParams.category}
-        search={searchParams.search}
-      />
+      <Suspense fallback={<LoadingCards />}>
+        <PropertiesContainer
+          category={searchParams.category}
+          search={searchParams.search}
+        />
+      </Suspense>
     </section>
   )
 }
--- components/card/LoadingCards.tsx
+++ components/card/LoadingCards.tsx
@@ -1,5 +1,24 @@
+import { Skeleton } from '../ui/skeleton'
+
 function LoadingCards() {
-  return <div>LoadingCards</div>
+  return (
+    <div className='mt-4 grid gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+      <SkeletonCard />
+      <SkeletonCard />
+      <SkeletonCard />
+      <SkeletonCard />
+    </div>
+  )
+}
+
+export function SkeletonCard() {
+  return (
+    <div>
+      <Skeleton className='h-[300px] rounded-md' />
+      <Skeleton className='mt-2 h-4 w-3/4' />
+      <Skeleton className='mt-2 h-4 w-1/2' />
+    </div>
+  )
 }
 
 export default LoadingCards
