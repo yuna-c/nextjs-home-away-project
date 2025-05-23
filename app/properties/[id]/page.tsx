7c71d8b6ea4f713ae0ffd0a94c9bb32ae1a38fa0
yuna <yuna744@gmail.com>
2025-05-24 00:29:01 +0900

    Share Button Component - npm i react-share (공유 라이브러리) - 트위터/링크드인/이메일 공유 팝업 UI 구현

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,5 +1,6 @@
 import FavoriteToggleButton from '@/components/card/FavoriteToggleButton'
 import BreadCrumbs from '@/components/properties/BreadCrumbs'
+import ShareButton from '@/components/properties/ShareButton'
 import { fetchPropertyDetails } from '@/utils/actions'
 import { redirect } from 'next/navigation'
 
@@ -17,6 +18,10 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
         <h1 className='text-4xl font-bold capitalize'>{property.tagline}</h1>
         <div className='flex items-center gap-x-4'>
           {/* share button */}
+          <ShareButton
+            name={property.name}
+            propertyId={property.id}
+          />
           <FavoriteToggleButton propertyId={property.id} />
         </div>
       </header>
---
+++ components/properties/ShareButton.tsx
@@ -0,0 +1,70 @@
+'use client'
+
+import { Button } from '../ui/button'
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
+import { LuShare2 } from 'react-icons/lu'
+import {
+  TwitterShareButton,
+  EmailShareButton,
+  LinkedinShareButton,
+  TwitterIcon,
+  EmailIcon,
+  LinkedinIcon
+} from 'react-share'
+
+function ShareButton({ propertyId, name }: { propertyId: string; name: string }) {
+  const url = process.env.NEXT_PUBLIC_WEBSITE_URL
+  const shareLink = `${url}/properties/${propertyId}`
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant='outline'
+          size='icon'
+          className='p-2'
+        >
+          <LuShare2 />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent
+        side='top'
+        align='end'
+        sideOffset={10}
+        className='flex w-full items-center justify-center gap-x-2'
+      >
+        <TwitterShareButton
+          url={shareLink}
+          title={name}
+        >
+          <TwitterIcon
+            size={32}
+            round
+          />
+        </TwitterShareButton>
+
+        <LinkedinShareButton
+          url={shareLink}
+          title={name}
+        >
+          <LinkedinIcon
+            size={32}
+            round
+          />
+        </LinkedinShareButton>
+
+        <EmailShareButton
+          url={shareLink}
+          title={name}
+        >
+          <EmailIcon
+            size={32}
+            round
+          />
+        </EmailShareButton>
+      </PopoverContent>
+    </Popover>
+  )
+}
+
+export default ShareButton
