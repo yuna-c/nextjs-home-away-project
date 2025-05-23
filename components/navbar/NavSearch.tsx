216b32dcaf8c2604854a9395d4cacac524e64b01
yuna <yuna744@gmail.com>
2025-05-23 14:24:49 +0900

    NavSearch Component - 검색창 입력값을 URL 쿼리에 반영하는 기능 구현 (검색 필터용) - use-debounce 설치 (입력 딜레이 처리용) - npm i use-debounce

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,11 +1,38 @@
+'use client'
+
 import { Input } from '../ui/input'
+import { useSearchParams, usePathname, useRouter } from 'next/navigation'
+import { useState, useEffect } from 'react'
+import { useDebouncedCallback } from 'use-debounce'
 
 function NavSearch() {
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
+  const { replace } = useRouter()
+
+  const [search, setSearch] = useState(searchParams.get('search')?.toString() || '')
+  const handleSearch = (value: string) => {
+    const params = new URLSearchParams(searchParams)
+    if (value) {
+      params.set('search', value)
+    } else {
+      params.delete('search')
+    }
+    replace(`${pathname}?${params.toString()}`)
+  }
+
+  useEffect(() => {}, [])
+
   return (
     <Input
       type='text'
       placeholder='find a property...'
       className='max-w-xs dark:bg-muted'
+      value={search}
+      onChange={(e) => {
+        setSearch(e.target.value)
+        handleSearch(e.target.value)
+      }}
     />
   )
 }
