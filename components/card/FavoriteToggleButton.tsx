4633bb8f7de5441561046c32c578f9f505307fa1
yuna <yuna744@gmail.com>
2025-05-23 18:56:40 +0900

    Fetch Favorite - 현재 유저의 숙소 좋아요 여부 확인 후 토글폼 연결

--- components/card/FavoriteToggleButton.tsx
+++ components/card/FavoriteToggleButton.tsx
@@ -1,20 +1,27 @@
 import { CardSignInButton } from '../form/SubmitButton'
 import { Button } from '../ui/button'
+import FavoriteToggleForm from './FavoriteToggleForm'
+import { fetchFavoriteId } from '@/utils/actions'
 import { auth } from '@clerk/nextjs/server'
 import { FaHeart } from 'react-icons/fa'
 
-function FavoriteToggleButton({ propertyId }: { propertyId: String }) {
+async function FavoriteToggleButton({ propertyId }: { propertyId: String }) {
   const { userId } = auth()
   if (!userId) return <CardSignInButton />
+  const favoriteId = await fetchFavoriteId({ propertyId })
 
   return (
-    <Button
-      size='icon'
-      variant='outline'
-      className='cursor-pointer p-2'
-    >
-      <FaHeart />
-    </Button>
+    <FavoriteToggleForm
+      favoriteId={favoriteId}
+      propertyId={propertyId}
+    />
+    // <Button
+    //   size='icon'
+    //   variant='outline'
+    //   className='cursor-pointer p-2'
+    // >
+    //   <FaHeart />
+    // </Button>
   )
 }
 
