54a99076e7762d2ed1956c912ab4a79cdff7778b
yuna <yuna744@gmail.com>
2025-05-28 00:21:03 +0900

    SubmitReviews Component - 리뷰 별점 UI 컴포넌트 제작

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -8,6 +8,7 @@ import ImageContainer from '@/components/properties/ImageContainer'
 import PropertyDetails from '@/components/properties/PropertyDetails'
 import ShareButton from '@/components/properties/ShareButton'
 import UserInfo from '@/components/properties/UserInfo'
+import SubmitReview from '@/components/reviews/SubmitReview'
 import { Skeleton } from '@/components/ui/skeleton'
 import { fetchPropertyDetails } from '@/utils/actions'
 import { Separator } from '@radix-ui/react-dropdown-menu'
@@ -80,6 +81,7 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
           <BookingCalendar />
         </div>
       </section>
+      <SubmitReview propertyId={property.id} />
     </section>
   )
 }
--- components/reviews/SubmitReview.tsx
+++ components/reviews/SubmitReview.tsx
@@ -1,5 +1,43 @@
-function SubmitReview() {
-  return <div>SubmitReview</div>
+'use client'
+
+import { SubmitButton } from '@/components/form/Buttons'
+import FormContainer from '@/components/form/FormContainer'
+import RatingInput from '@/components/form/RatingInput'
+import TextAreaInput from '@/components/form/TextAreaInput'
+import { Button } from '@/components/ui/button'
+import { Card } from '@/components/ui/card'
+import { createReviewAction } from '@/utils/actions'
+import { useState } from 'react'
+
+function SubmitReview({ propertyId }: { propertyId: string }) {
+  const [isReviewFormVisible, setIsReviewFormVisible] = useState(false)
+
+  return (
+    <div className='mt-8'>
+      <Button onClick={() => setIsReviewFormVisible((prev) => !prev)}>Leave a Review</Button>
+      {isReviewFormVisible && (
+        <Card className='mt-8 p-8'>
+          <FormContainer action={createReviewAction}>
+            <input
+              type='hidden'
+              name='propertyId'
+              value={propertyId}
+            />
+            <RatingInput name='rating' />
+            <TextAreaInput
+              name='comment'
+              labelText='your thoughts on this property'
+              defaultValue='Amazing place!!!'
+            />
+            <SubmitButton
+              text='Submit'
+              className='mt-4'
+            />
+          </FormContainer>
+        </Card>
+      )}
+    </div>
+  )
 }
 
 export default SubmitReview
