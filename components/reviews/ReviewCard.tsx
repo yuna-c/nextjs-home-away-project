23fedc9542755b40b02fa44eb06cfc8bc8a97164
yuna <yuna744@gmail.com>
2025-05-28 08:28:35 +0900

    ReviewCard Component - 프로필 이미지, 이름, 별점, 코멘트 출력 - Rating, Comment 컴포넌트 조합 - children을 통해 delete 버튼 등 삽입 가능

--- components/reviews/ReviewCard.tsx
+++ components/reviews/ReviewCard.tsx
@@ -1,5 +1,40 @@
-function ReviewCard() {
-  return <div>ReviewCard</div>
+import Comment from './Comment'
+import Rating from './Rating'
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
+
+type ReviewCardProps = {
+  reviewInfo: {
+    comment: string
+    rating: number
+    name: string
+    image: string
+  }
+  children?: React.ReactNode
+}
+
+function ReviewCard({ reviewInfo, children }: ReviewCardProps) {
+  return (
+    <Card className='relative'>
+      <CardHeader>
+        <div className='flex items-center'>
+          <img
+            src={reviewInfo.image}
+            alt='profile'
+            className='h-12 w-12 rounded-full object-cover'
+          />
+          <div className='ml-4'>
+            <h3 className='mb-1 text-sm font-bold capitalize'>{reviewInfo.name}</h3>
+            <Rating rating={reviewInfo.rating} />
+          </div>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <Comment comment={reviewInfo.comment} />
+      </CardContent>
+      {/* delete button later */}
+      <div className='absolute right-3 top-3'>{children}</div>
+    </Card>
+  )
 }
 
 export default ReviewCard
