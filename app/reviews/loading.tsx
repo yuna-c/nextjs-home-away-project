5eb9dffde28e3c18945bbd6baedc25773eded391
yuna <yuna744@gmail.com>
2025-05-30 19:08:12 +0900

    Reviews Loading Component - 리뷰 로딩 skeleton 제작

---
+++ app/reviews/loading.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
+import { Skeleton } from '@/components/ui/skeleton'
+
+function loading() {
+  return (
+    <section className='mt-4 grid gap-8 md:grid-cols-2'>
+      <ReviewLoadingCard />
+      <ReviewLoadingCard />
+    </section>
+  )
+}
+
+const ReviewLoadingCard = () => {
+  return (
+    <Card>
+      <CardHeader>
+        <div className='flex items-center'>
+          <Skeleton className='h-12 w-12 rounded-full' />
+          <div className='ml-4'>
+            <Skeleton className='mb-2 h-4 w-[150px]' />
+            <Skeleton className='h-4 w-[100px]' />
+          </div>
+        </div>
+      </CardHeader>
+    </Card>
+  )
+}
+
+export default loading
