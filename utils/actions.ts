0b70a7604c073e241b1b80149c2d4a5785f96bcd
yuna <yuna744@gmail.com>
2025-06-01 22:54:38 +0900

    PropertyRating Component - Complete - fetchPropertyRating를 통한 별점/ 댓글 평균

--- utils/actions.ts
+++ utils/actions.ts
@@ -385,3 +385,19 @@ export const deleteReviewAction = async (prevState: { reviewId: string }) => {
 export const updateReviewAction = async () => {
   return { message: 'update review' }
 }
+
+export async function fetchPropertyRating(propertyId: string) {
+  const result = await db.review.groupBy({
+    by: ['propertyId'],
+    _avg: {
+      rating: true
+    },
+    _count: {
+      rating: true
+    },
+    where: {
+      propertyId
+    }
+  })
+  return { rating: result[0]?._avg.rating?.toFixed() ?? 0, count: result[0]?._count.rating ?? 0 }
+}
