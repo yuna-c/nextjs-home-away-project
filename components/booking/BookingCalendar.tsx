8f2ae383833980468d4aa7ce5279476f797c6c1a
yuna <yuna744@gmail.com>
2025-06-02 03:50:24 +0900

    Booking Container / Calendar - Initial Setup - BookingCalendar 초기 셋업 및 날짜 범위 Zustand 상태 연동

--- components/booking/BookingCalendar.tsx
+++ components/booking/BookingCalendar.tsx
@@ -1,5 +1,30 @@
+'use client'
+
+import { Calendar } from '@/components/ui/calendar'
+import { useToast } from '@/components/ui/use-toast'
+import { generateDisabledDates, generateDateRange, defaultSelected, generateBlockedPeriods } from '@/utils/calendar'
+import { useProperty } from '@/utils/store'
+import { useEffect, useState } from 'react'
+import { DateRange } from 'react-day-picker'
+
 function BookingCalendar() {
-  return <div>BookingCalendar</div>
+  const currentDate = new Date()
+  const [range, setRange] = useState<DateRange | undefined>(defaultSelected)
+
+  useEffect(() => {
+    // setState: Zustand 전역 상태 업데이트 함수 (부분 업데이트)
+    useProperty.setState({ range })
+  }, [range])
+
+  return (
+    <Calendar
+      mode='range'
+      defaultMonth={currentDate}
+      selected={range}
+      onSelect={setRange}
+      className='mb-4'
+    />
+  )
 }
 
 export default BookingCalendar
--- components/booking/BookingContainer.tsx
+++ components/booking/BookingContainer.tsx
@@ -1,5 +1,19 @@
+'use client'
+
+import BookingForm from './BookingForm'
+import ConfirmBooking from './ConfirmBooking'
+import { useProperty } from '@/utils/store'
+
 function BookingContainer() {
-  return <div>BookingContainer</div>
+  const state = useProperty((state) => state)
+  console.log(state)
+
+  return (
+    <div className='w-full'>
+      <BookingForm />
+      <ConfirmBooking />
+    </div>
+  )
 }
 
 export default BookingContainer
