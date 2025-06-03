0b946a7242cf1241f8ec9f75d1a08bd1e54c73fd
yuna <yuna744@gmail.com>
2025-06-03 22:14:07 +0900

    Disabled Dates - 예약 중복 방지

--- components/booking/BookingCalendar.tsx
+++ components/booking/BookingCalendar.tsx
@@ -1,21 +1,38 @@
-'use client'
+'use client';
 
-import { Calendar } from '@/components/ui/calendar'
-import { useToast } from '@/components/ui/use-toast'
-import { generateDisabledDates, generateDateRange, defaultSelected, generateBlockedPeriods } from '@/utils/calendar'
-import { useProperty } from '@/utils/store'
-import { useEffect, useState } from 'react'
-import { DateRange } from 'react-day-picker'
+import { Calendar } from '@/components/ui/calendar';
+import { useToast } from '@/hooks/use-toast';
+import { generateDisabledDates, generateDateRange, defaultSelected, generateBlockedPeriods } from '@/utils/calendar';
+import { useProperty } from '@/utils/store';
+import { useEffect, useState } from 'react';
+import { DateRange } from 'react-day-picker';
+
 
 function BookingCalendar() {
   const currentDate = new Date()
   const [range, setRange] = useState<DateRange | undefined>(defaultSelected)
   const bookings = useProperty((state) => state.bookings)
-  console.log(bookings)
-
+  // console.log(bookings)
+  const { toast } = useToast()
+  // 오늘 이전 날짜는 선택 못 하게
   const blockedPeriods = generateBlockedPeriods({ bookings, today: currentDate })
+  // 예약 불가 날짜 단위 선택 못하게
+  const unavailableDates = generateDisabledDates(blockedPeriods)
+  console.log(unavailableDates)
 
   useEffect(() => {
+    const selectedRange = generateDateRange(range) // 날짜 범위를 배열로 변환
+    const isDisabledDateIncluded = selectedRange.some((date) => {
+      if (unavailableDates[date]) {
+        setRange(defaultSelected) // 다시 선택하게 초기화
+        toast({
+          description: 'Some dates are booked. Please select again'
+        })
+        return true
+      }
+      return false
+    })
+
     // setState: Zustand 전역 상태 업데이트 함수 (부분 업데이트)
     useProperty.setState({ range })
   }, [range])
