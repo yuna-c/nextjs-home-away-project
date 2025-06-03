6a147a2fd1ba94b12f22f8be391b22e25a78d680
yuna <yuna744@gmail.com>
2025-06-04 00:05:52 +0900

    Delete Booking - 예약 삭제 버튼 제작

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -51,13 +51,25 @@ async function BookingsPage() {
                 <TableCell>{formatCurrency(orderTotal)}</TableCell>
                 <TableCell>{startDate}</TableCell>
                 <TableCell>{endDate}</TableCell>
-                <TableCell></TableCell>
+                <TableCell>
+                  <DeleteBooking bookingId={id} />
+                </TableCell>
               </TableRow>
             )
           })}
         </TableBody>
       </Table>
     </div>
+  )
+}
+
+function DeleteBooking({ bookingId }: { bookingId: string }) {
+  const deleteBooking = deleteBookingAction.bind(null, { bookingId })
+
+  return (
+    <FormContainer action={deleteBooking}>
+      <IconButton actionType='delete' />
+    </FormContainer>
   )
 }
 export default BookingsPage
