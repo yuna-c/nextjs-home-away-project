2958ea72d1f264d196f5301c89769080df87d89f
yuna <yuna744@gmail.com>
2025-06-09 23:00:21 +0900

    Stripe - Checkout Page - 결제 페이지 구현 (클라이언트 컴포넌트 + Embedded Checkout 연동)

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,4 +1,39 @@
+'use client'
+
+import { EmbeddedCheckoutProvider, EmbeddedCheckout } from '@stripe/react-stripe-js'
+import { loadStripe } from '@stripe/stripe-js'
+import axios from 'axios'
+import { useSearchParams } from 'next/navigation'
+import React, { useCallback } from 'react'
+
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string)
+
 function CheckoutPage() {
-  return <h1 className="text-3xl">CheckoutPage</h1>
+  // URL 쿼리에서 bookingId를 추출
+  const searchParams = useSearchParams()
+  const bookingId = searchParams.get('bookingId')
+
+  // Stripe Checkout 세션을 생성하기 위한 백엔드 API 요청
+  const fetchClientSecret = useCallback(async () => {
+    const response = await axios.post('/api/payment', {
+      bookingId: bookingId
+    })
+    return response.data.clientSecret
+  }, [])
+
+  // EmbeddedCheckoutProvider에 넘길 옵션
+  const options = { fetchClientSecret }
+
+  return (
+    <div id='checkout'>
+      {/* 실제 Stripe 결제 창 임베디드(내장) */}
+      <EmbeddedCheckoutProvider
+        stripe={stripePromise} // 세션 인증
+        options={options}
+      >
+        <EmbeddedCheckout />
+      </EmbeddedCheckoutProvider>
+    </div>
+  )
 }
 export default CheckoutPage
